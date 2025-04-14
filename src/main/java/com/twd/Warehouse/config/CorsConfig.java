172733626f9364d1921615d1420322f541db5241
youssefmabrouk123 <130383280+youssefmabrouk123@users.noreply.github.com>
2025-04-14 21:16:20 +0100

    Update CorsConfig.java

--- src/main/java/com/twd/Warehouse/config/CorsConfig.java
+++ src/main/java/com/twd/Warehouse/config/CorsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
 
@@ -13,11 +12,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Autorise tous les endpoints
-                        .allowedOrigins("http://localhost:4200") // Origine de votre frontend
-                        .allowedMethods("   GET", "POST", "PUT", "DELETE") // Méthodes HTTP autorisées
-                        .allowCredentials(true) // Autorise les credentials (cookies, etc.)
-                        .allowedHeaders("*"); // Autorise tous les headers (optionnel mais recommandé)
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // Permet d'utiliser * avec allowCredentials
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
             }
         };
     }
